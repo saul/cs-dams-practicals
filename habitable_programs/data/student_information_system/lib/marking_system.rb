1caf4b062fdbfce1fb6e5034c51219ed671a4720
Saul Rennison <saul.rennison@gmail.com>
2015-11-23 11:27:57 +0000

    Refactored Student Information System

--- habitable_programs/data/student_information_system/lib/marking_system.rb
+++ habitable_programs/data/student_information_system/lib/marking_system.rb
@@ -1,24 +1,63 @@
 require_relative "york/student_info"
 
+class Assessment
+  def initialize(registry:, assessment:)
+    @registry = registry
+    @assessment = assessment
+  end
+
+  def add_mark(student_number, mark)
+    @registry.add_mark(@assessment, student_number, mark)
+  end
+
+  def finalise
+    @registry.finalise_assessment(@assessment)
+  end
+end
+
+class Registry
+  def initialize
+    @instance = York::StudentInfo.instance
+  end
+
+  def find_module(module_code, create_if_not_found)
+    @instance.find_module(module_code, create_if_not_found)
+  end
+
+  def create_assessment(mod, year)
+    ass = @instance.create_assessment(mod, year)
+    Assessment.new(registry: @instance, assessment: ass)
+  end
+
+  def find_assessment(module_code, year)
+    mod = find_module(module_code, false)
+
+    unless mod.nil?
+      mod.assessments.find { |ass| ass.year == year }
+    end
+  end
+end
+
 class MarkingSystem
+  def initialize(registry:Registry.new)
+    @registry = registry
+  end
+
   def add_marks(module_code, year, marks)
-    registry = York::StudentInfo.instance
-    mod = registry.find_module(module_code, true)
-    assessment = registry.create_assessment(mod, year)
+    mod = @registry.find_module(module_code, true)
+    assessment = @registry.create_assessment(mod, year)
 
     marks.each do |student_number, mark|
-      registry.add_mark(assessment, student_number, mark)
+      assessment.add_mark(student_number, mark)
     end
 
-    registry.finalise_assessment(assessment)
+    assessment.finalise
   end
 
   def summarise_marks(module_code, year)
-    registry = York::StudentInfo.instance
-    mod = registry.find_module(module_code, false)
-    return "" if mod.nil?
+    ass = @registry.find_assessment(module_code, year)
+    return "" if ass.nil?
 
-    marks = mod.assessments.find { |ass| ass.year == year }.marks
-    marks.map { |student_number, mark| "#{student_number}: #{mark}" }.join("\n")
+    ass.marks.map { |student_number, mark| "#{student_number}: #{mark}" }.join("\n")
   end
 end
